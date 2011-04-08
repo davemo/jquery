a2e3f1a068cb111ba35836403f9f4a1bfa74f8b9
David Mosher <davidmosher@gmail.com>
2011-04-07 22:58:39 -0600

    initial crack at getting core tests running in jasmine

---
+++ test/jasmine/spec/core.jasmine.js
@@ -0,0 +1,57 @@
+describe('Basic Requirements', function() {
+		
+	describe("Array.push()", function() {
+		it("is defined", function() {
+			expect(Array.prototype.push).toBeDefined();
+			
+		});
+	});
+
+	describe("Function.apply()", function() {
+		it("is defined", function() {
+			expect(Function.prototype.apply).toBeDefined();
+		});
+	});
+
+	describe("document.getElementById", function() {
+	  it("is defined", function() {
+	    expect(document.getElementById).toBeDefined();
+	  });
+	});
+
+	describe("document.getElementsByTagName", function() {
+	  it("is defined", function() {
+	    expect(document.getElementsByTagName).toBeDefined();
+	  });
+	});
+
+	describe("RegExp", function() {
+	  it("is defined", function() {
+	    expect(RegExp).toBeDefined();
+	  });
+	});
+
+	describe("jQuery", function() {
+	  it("is defined", function() {
+	    expect(jQuery).toBeDefined();
+	  });
+	});
+
+	describe("$", function() {
+	  it("is defined", function() {
+	    expect($).toBeDefined();
+	  });
+	});
+				
+});
+
+//test("Basic requirements", function() {
+//	expect(7);
+//	ok( Array.prototype.push, "Array.push()" );
+//	ok( Function.prototype.apply, "Function.apply()" );
+//	ok( document.getElementById, "getElementById" );
+//	ok( document.getElementsByTagName, "getElementsByTagName" );
+//	ok( RegExp, "RegExp" );
+//	ok( jQuery, "jQuery" );
+//	ok( $, "$" );
+//});
